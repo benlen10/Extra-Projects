040d66fb412e421ddaeccd42b9f2101d2d724a65
benlen10 <ben2lening@gmail.com>
2015-04-13 08:29:40 -0400

    Update TxtEncryptor

--- C/TxtEncryptor/TxtEncryptor.cpp
+++ C/TxtEncryptor/TxtEncryptor.cpp
@@ -12,16 +12,46 @@ void encrypt(char *s)
         s[i] -= num;
 }
 
+void decrypt(char *s)
+{
+    int i, l = strlen(s);
+    for(i = 0; i < l; i++)
+        s[i] += num;
+}
+
+
+
+
+
 main(){
-	printf("Type a string to encrypt. Press ENTER to confirm\n");
 	char * str = (char*) malloc(100);
-	
+	printf("Type your option and press enter:\n");
+	printf("ENCRYPT or DECRYPT\n");
+	scanf("%s", str);
+	if(strcmp(str,"ENCRYPT")==0){
+		printf("Type a string to encrypt. Press ENTER to confirm\n");
 	scanf("%s", str);
 	printf("Type an encryption number\n");
 	scanf("%d",&num);
 	encrypt(str);
 	printf("Encrypted String: %s\n",str);
+	return 0;
+	}
+	else if(strcmp(str,"DECRYPT")==0){
+		printf("Type a string to decrypt. Press ENTER to confirm\n");
+	scanf("%s", str);
+	printf("Type an decryption number\n");
+	scanf("%d",&num);
+	decrypt(str);
+	printf("Decrypted String: %s\n",str);
+	}
+	else{
+		printf("Invalid option\n");
+		return 1;
+	}
+	return 0;
 }
+	
 	
 	
 	
