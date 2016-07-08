5d8d3a34c7b558c4c0582f2d78eb85f2862d0924
Ben Lenington <UC214636@ten.thomsonreuters.com>
2016-07-08 15:02:07 -0500

    GuestManagment
    Add GuestManagment to finished Projects

---
+++ C/GuestManagment (Prototype)/guest.h
@@ -0,0 +1,19 @@
+#ifndef GUEST
+#define GUEST
+#include <string>
+
+using namespace std;
+
+
+class Guest {
+	
+	public:
+	char name[100];
+	int date;
+	int status;
+	char state[100];
+	
+	
+};
+#endif
+	
