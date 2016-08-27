db4002c7ec7dc0d9174b6f66d15379ca0214cf43
Ben Lenington <ben2lening@gmail.com>
2016-08-27 12:23:21 -0500

    Move UniCade Interface
    - Move UniCade Interface source and release folders to root directory

---
+++ UniCade Interface (V1.33 Source)/UniCade/Backend/Database.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace UniCade
+{
+    public static class Database
+    {
+        #region Properties
+
+        public static List<Console> ConsoleList { get; set; }
+        public static List<User> UserList { get; set; }
+        public static int TotalGameCount { get; set; }
+        public static string HashKey { get; set; }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Initialize the static database members
+        /// </summary>
+        public static void Initialize()
+        {
+            TotalGameCount = 0;
+            HashKey = null;
+            ConsoleList = new List<Console>();
+            UserList = new List<User>();
+        }
+
+        #endregion
+    }
+}
---
+++ UniCade Interface (V1.33 Source)/UniCade/Windows/PassWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UniCade
+{
+    public partial class PassWindow : Form
+    {
+        public PassWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Enter button. Check if the entered pass is valid, otherwise display an error
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            Int32.TryParse(textBox1.Text, out n);
+            if (n > 0)
+            {
+                if (Int32.Parse(textBox1.Text) == SettingsWindow.passProtect)
+                {
+                    DialogResult = DialogResult.OK;
+                    MainWindow._validPAss = true;
+                }
+
+            }
+            textBox1.Text = null;
+        }
+
+        /// <summary>
+        /// Close button
+        /// </summary>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
